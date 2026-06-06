47e117431c7ecd79ee102497fe2e67840f234cd9
來紀充 <mitsuru@mitsurunoMacBook-Air.local>
2026-06-07 05:50:11 +0900

    reduce maxOutputTokens for vercel timeout

--- api/gemini.js
+++ api/gemini.js
@@ -27,7 +27,7 @@ export default async function handler(req, res) {
         generationConfig: {
           temperature: 0.4,
           topP: 0.9,
-          maxOutputTokens: 2000,
+          maxOutputTokens: 1000,
         },
       }),
     });
